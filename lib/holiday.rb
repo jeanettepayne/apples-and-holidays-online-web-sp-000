9a73d8452a072071df206aad3434280512019b0b
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-17 00:43:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
    puts "#{season.capitalize}:"
    season.each do |holiday, supplies|
     # holiday.to_s.split.capitalize!.join(" ")
-     puts "#{holiday.to_s.split.capitalize}: "
+     puts "#{holiday.split.to_s.capitalize}: "
      supplies.each do |array|
        array.flatten
      end
