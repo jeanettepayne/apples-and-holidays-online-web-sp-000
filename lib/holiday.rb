5a184c8b4146c5373f1af1bd99ca664a1c611c24
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-17 01:16:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
  holiday_hash.each do |season, holiday|
    holiday.each do |holiday_name, supplies|
-     if supplies.include? == "BBQ"
+     if supplies.include?("BBQ")
        puts "#{holiday_name}"
      end
    end
